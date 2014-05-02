19b6eb1c2f3a4e28d81f589870e064a3de3bfca0
robchico <robertochicoperez@gmail.com>
2014-05-02 17:17:19 +0200

    primar cambio de la rama Ej5-5

--- Ejercicio5/src/ej5/Ejer5.java
+++ Ejercicio5/src/ej5/Ejer5.java
@@ -1,10 +1,21 @@
 package ej5;
-
+/**
+ * 
+ * @author admin
+ *
+ */
 public class Ejer5 {
-
+/**
+ * 
+ */
 	public Ejer5(){
 		System.out.println("solo es eun ejercicio para usar GITHUB");
 	}
+	/**
+	 * 
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
 		new Ejer5();
 		System.exit(0);
