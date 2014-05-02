1a05e76367803c73b6d85b451a33ca746135e4ac
Rob <roberto@>
2014-05-02 12:31:16 +0200

    segunda version

--- Ejercicio5/src/ej5/Ejer5.java
+++ Ejercicio5/src/ej5/Ejer5.java
@@ -3,7 +3,11 @@ package ej5;
 public class Ejer5 {
 
 	public Ejer5(){
+		/**
+		 * cambio1
+		 */
 		System.out.println("solo es eun ejercicio para usar GITHUB");
+		
 	}
 	public static void main(String[] args) {
 		new Ejer5();
